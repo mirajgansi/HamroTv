d6206baa221dd63003c3bbea58463d800043ab37
mirajgansi <mirajgansi122@gmail.com>
2024-12-31 13:01:29 +0545

    Resolved merge conflicts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <App />
    
-   <Signup />
+   <Login />
   </React.StrictMode>
 );
 
